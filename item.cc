c5be0eacd32ea872b95fe993762313281eeaa287
cj2zhang <cj2zhang@uwaterloo.ca>
2025-03-24 21:07:32 -0400

    added declarations to item.cc

---
+++ item.cc
@@ -0,0 +1,13 @@
+export module item;
+
+import info;
+import playablecharacter;
+using namespace std;
+
+export class Item {
+    int x;
+    int y;
+    public:
+        virtual Info getInfo() = 0;
+        virtual void onPickup() = 0;
+};
