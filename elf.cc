c9317ab336c158d9fd29e27516203da45a86adaa
j2tedjos <j2tedjos@uwaterloo.ca>
2025-03-26 19:56:50 -0400

    Made ctors for all base character classes

---
+++ elf.cc
@@ -0,0 +1,8 @@
+export module elf;
+import playablecharacter;
+
+export class Elf: public PC {
+    public:
+    Human(int x, int y): 
+        health(140), attack(30), defense(10), gold(0), x(x), y(y), hasBS(false) {}
+}
---
+++ troll.cc
@@ -0,0 +1,8 @@
+export module troll;
+import enemy;
+
+export class Troll: public Enemy {
+    public:
+    Phoenix(int x, int y): 
+        health(120), attack(25), defense(15), gold(1), x(x), y(y), wasMoved(false), hasCompass(false) {}
+}
