2a6a6f0ab0d9a7ca3f3c7406565a7a6b9d3ec63f
cj2zhang <cj2zhang@uwaterloo.ca>
2025-03-26 02:25:51 -0400

    added classes for TextDisplay

---
+++ textdisplay.cc
@@ -0,0 +1,27 @@
+export module textdisplay;
+
+import <cstddef>
+import <iostream>;
+import <vector>;
+import observer;
+
+class GameObject;
+
+export class TextDisplay: public Observer {
+    std::vector<std::vector<char>> theDisplay;
+    bool displayStairs;
+    std::vector<bool> potionSeen;
+
+ public:
+    explicit TextDisplay( std::size_t n );
+    ~TextDisplay();
+
+
+    // GameObject/Map tells text display that status for the game object has changed and needs to be updated.
+    virtual void notify( Subject & whoNotified ) override;
+
+    // Outputs the map of chars.
+    friend std::ostream &operator<<( std::ostream &out, const TextDisplay &td );
+};
+
+std::ostream &operator<<( std::ostream &out, const TextDisplay &td );
