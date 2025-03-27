1b45500a7989ed8507f3b4da2b6ded1c1287267e
j2tedjos <j2tedjos@uwaterloo.ca>
2025-03-26 20:08:45 -0400

    Fixed a mistake on werewolf.cc

--- werewolf.cc
+++ werewolf.cc
@@ -3,6 +3,6 @@ import enemy;
 
 export class Werewolf: public Enemy {
     public:
-    Phoenix(int x, int y): 
+    Werewolf(int x, int y): 
         health(120), attack(30), defense(5), gold(1), x(x), y(y), wasMoved(false), hasCompass(false) {}
 }
